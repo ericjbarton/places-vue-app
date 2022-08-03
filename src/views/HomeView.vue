bd62af286f515a0877be34e9ae4e6e73d8cfba86
ericjbarton <erockbart@gmail.com>
2022-08-03 08:43:58 -0400

    adds created function, methods to HomeView, things get dicey

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -13,4 +13,19 @@ export default {
   created: function () {
     this.indexPlaces();
   },
-
+  methods: {
+    indexPlaces: function () {
+      axios.get("http://localhost:3000/places").then((response) => {
+        this.places = response.data;
+        console.log("All Phantom Towns", this.places);
+      });
+    },
+        createPlace: function () {
+      axios
+        .post("http://localhost:3000/places", this.newPlaceParams)
+        .then((response) => {
+          console.log("Place created ", response.data);
+          this.places.push(response.data);
+          this.newPlaceParams = {};
+          this.showErrorMessage = false;
+        
